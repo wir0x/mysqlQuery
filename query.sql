9228e27ba01cc7c57a3463fe13d80c3075f883b0
gonzalosv <gonzalo.salazar@swissbytes.ch>
2015-12-03 09:11:38 -0400

    add new procedure

--- query.sql
+++ query.sql
@@ -1,3 +1,16 @@
+DROP PROCEDURE IF EXISTS buhodb.delete_smartphone_with_imei;
+CREATE DEFINER=`root`@`%` PROCEDURE `delete_smartphone_with_imei`(IN p_imei VARCHAR(255))
+BEGIN
+    DELETE from positions WHERE device_id in (select id from device where imei = p_imei);
+    DELETE from positions_buffer WHERE device_id in (select id from device where imei = p_imei);
+    delete from user_device where device_id in (select id from device where imei = p_imei);
+    delete from subscription where device_id in (select id from device where imei = p_imei);
+#     delete from payment_request_item where payment_request_id in (select id from payment_request where device_id in (select id from device where imei = p_imei));
+#     delete from payment_request where device_id in (select id from device where imei = p_imei);
+    delete from device where imei = p_imei;
+  END;
+
+
 SET @deviceId = 112;
 
 DELETE FROM positions WHERE device_id =  @deviceId ;
